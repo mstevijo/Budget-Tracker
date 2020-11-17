c3f17ecef2f546549125020b83ae77f222fb902e
Stevi Jo Malinowski <mstevijo@yahoo.com>
2020-11-16 20:39:01 -0800

    worked on all files

--- public/index.js
+++ public/index.js
@@ -1,3 +1,11 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js").then(reg => {
+      console.log("We found your service worker file!", reg);
+    });
+  });
+}
+
 let transactions = [];
 let myChart;
 
@@ -151,3 +159,4 @@ document.querySelector("#add-btn").onclick = function() {
 document.querySelector("#sub-btn").onclick = function() {
   sendTransaction(false);
 };
+
