ebcb2e1b4c9649ccaeb3c2153946b20b82290e1f
Stevi Jo Malinowski <mstevijo@yahoo.com>
2020-11-16 22:23:38 -0800

    saved and exit

--- public/index.js
+++ public/index.js
@@ -1,10 +1,10 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js").then(reg => {
-      console.log("We found your service worker file!", reg);
-    });
-  });
-}
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker.register("/service-worker.js").then(reg => {
+//       console.log("We found your service worker file!", reg);
+//     });
+//   });
+// }
 
 let transactions = [];
 let myChart;
